8658d1d3d2a4555c1ca37bc6f372e29e89848bd8
Carlos Coll <collhuer@gmail.com>
2025-02-10 12:27:52 +0100

    Improvements GUI qr dialog

--- lib/widgets/qr_dialog.dart
+++ lib/widgets/qr_dialog.dart
@@ -1,137 +1,175 @@
 // lib/widgets/qr_dialog.dart
 import 'package:flutter/material.dart';
-import 'package:fpt_app/services/api_service.dart';
 import 'package:fpt_app/config.dart';
 import 'package:fpt_app/screens/login_screen.dart';
+import 'package:fpt_app/services/user_service.dart';
 import 'package:fpt_app/widgets/qr_scanner.dart';
 import 'package:qr_flutter/qr_flutter.dart';
 
 class QrDialog extends StatefulWidget {
-  const QrDialog({super.key});
+  const QrDialog({Key? key}) : super(key: key);
+
   @override
   _QrDialogState createState() => _QrDialogState();
 }
 
 class _QrDialogState extends State<QrDialog> {
-  final ApiService _apiService = ApiService(baseUrl: Config.BASE_URL);
+  final UserService _userService = UserService(baseUrl: Config.BASE_URL);
 
   @override
   Widget build(BuildContext context) {
-    return SimpleDialog(
-      backgroundColor: Theme.of(context).colorScheme.secondary,
-      children: <Widget>[
-        Column(
-          children: <Widget>[
-            TextButton(
-              style: TextButton.styleFrom(
-                foregroundColor: Theme.of(context).colorScheme.primary,
-              ),
+    return Dialog(
+      backgroundColor: Colors.transparent,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(16),
+      ),
+      child: Padding(
+        padding: const EdgeInsets.all(16),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            ElevatedButton.icon(
               onPressed: () {
+                // Lógica para escanear QR
                 showDialog(
                   context: context,
-                  builder: (context) => Container(
-                    alignment: Alignment.center,
+                  builder: (context) => Dialog(
+                    backgroundColor: Theme.of(context).colorScheme.secondary,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(16),
+                    ),
                     child: Container(
                       height: 400,
                       width: 600,
-                      margin: const EdgeInsets.all(20),
-                      padding: const EdgeInsets.all(2),
-                      decoration: BoxDecoration(
-                        color: Theme.of(context).colorScheme.secondary,
-                        borderRadius: BorderRadius.circular(10),
-                      ),
-                      child: Column(
-                        mainAxisSize: MainAxisSize.min,
-                        children: [
-                          Flexible(
-                            child: ClipRRect(
-                              borderRadius: BorderRadius.circular(10),
-                              child: QrScannerWidget(
-                                onScanSuccess: (String? code) {
-                                  _apiService.consumeDrink(code).then((response) {
-                                    // Procesa la respuesta si es necesario
-                                  }).catchError((error) {
-                                    if (error is SessionExpiredException) {
-                                      Navigator.pushReplacement(
-                                        context,
-                                        MaterialPageRoute(builder: (context) => const LoginScreen()),
-                                      );
-                                    }
-                                  });
-                                  Navigator.of(context).pop(code);
-                                },
-                              ),
-                            ),
-                          ),
-                        ],
+                      padding: const EdgeInsets.all(16),
+                      child: QrScannerWidget(
+                        onScanSuccess: (String? code) {
+                          _userService.consumeDrink(code).then((response) {
+                            // Procesa la respuesta si es necesario
+                          }).catchError((error) {
+                            if (error is SessionExpiredException) {
+                              Navigator.pushReplacement(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (context) => const LoginScreen()),
+                              );
+                            }
+                          });
+                          Navigator.of(context).pop(code);
+                        },
                       ),
                     ),
                   ),
                 );
               },
-              child: const Text(
-                'ESCANEAR',
-                style: TextStyle(fontSize: 20),
+              icon: const Icon(
+                Icons.qr_code_scanner,
+                size: 32, // Tamaño mayor para el ícono
+              ),
+              label: const Text(
+                'Escanear',
+                style: TextStyle(fontSize: 20), // Fuente más grande
+              ),
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Theme.of(context).colorScheme.secondary,
+                padding: const EdgeInsets.symmetric(vertical: 24, horizontal: 32),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(12),
+                ),
               ),
             ),
-            const Divider(color: Colors.black45, thickness: 1),
-            TextButton(
-              style: TextButton.styleFrom(
-                foregroundColor: Theme.of(context).colorScheme.primary,
-              ),
+            const SizedBox(height: 16),
+            ElevatedButton.icon(
               onPressed: () {
+                // Lógica para generar QR
                 showDialog(
                   context: context,
                   builder: (BuildContext context) {
-                    return SimpleDialog(
-                      children: [
-                        SizedBox(
-                          width: 320,
-                          height: 320,
-                          child: FutureBuilder<String>(
-                            future: _apiService.createQrCodeData(),
-                            builder: (BuildContext context, AsyncSnapshot<String> snapshot) {
-                              if (snapshot.connectionState == ConnectionState.waiting) {
-                                return const CircularProgressIndicator();
-                              } else if (snapshot.hasError) {
-                                if (snapshot.error is SessionExpiredException) {
-                                  WidgetsBinding.instance.addPostFrameCallback((_) {
-                                    Navigator.pushReplacement(
-                                      context,
-                                      MaterialPageRoute(builder: (context) => const LoginScreen()),
-                                    );
-                                  });
-                                  return Container();
-                                }
-                                return const Center(child: Text('Uh oh! Something went wrong...', textAlign: TextAlign.center));
-                              } else if (snapshot.hasData) {
-                                return QrImageView(
-                                  data: snapshot.data!,
-                                  version: QrVersions.auto,
-                                  gapless: false,
-                                  errorStateBuilder: (cxt, err) {
-                                    return const Center(child: Text('Uh oh! Something went wrong...', textAlign: TextAlign.center));
-                                  },
-                                );
-                              } else {
-                                return const Center(child: Text("No QR data available"));
+                    return Dialog(
+                      backgroundColor: Theme.of(context).colorScheme.secondary,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(16),
+                      ),
+                      child: Container(
+                        width: 320,
+                        height: 320,
+                        padding: const EdgeInsets.all(16),
+                        child: FutureBuilder<String>(
+                          future: _userService.createQrCodeData(),
+                          builder: (BuildContext context, AsyncSnapshot<String> snapshot) {
+                            if (snapshot.connectionState == ConnectionState.waiting) {
+                              return const Center(child: CircularProgressIndicator());
+                            } else if (snapshot.hasError) {
+                              if (snapshot.error is SessionExpiredException) {
+                                WidgetsBinding.instance.addPostFrameCallback((_) {
+                                  Navigator.pushReplacement(
+                                    context,
+                                    MaterialPageRoute(
+                                        builder: (context) => const LoginScreen()),
+                                  );
+                                });
+                                return Container();
                               }
-                            },
-                          ),
+                              return const Center(
+                                  child: Text(
+                                    '¡Vaya! Algo ha salido mal...',
+                                    textAlign: TextAlign.center,
+                                  ));
+                            } else if (snapshot.hasData) {
+                              return QrImageView(
+                                data: snapshot.data!,
+                                version: QrVersions.auto,
+                                gapless: false,
+                                errorStateBuilder: (cxt, err) {
+                                  return const Center(
+                                    child: Text(
+                                      '¡Vaya! Algo ha salido mal...',
+                                      textAlign: TextAlign.center,
+                                    ),
+                                  );
+                                },
+                              );
+                            } else {
+                              return const Center(child: Text("No hay datos QR disponibles"));
+                            }
+                          },
                         ),
-                      ],
+                      ),
                     );
                   },
                 );
               },
-              child: const Text(
-                'GENERAR',
-                style: TextStyle(fontSize: 20),
+              icon: const Icon(
+                Icons.qr_code,
+                size: 32, // Ícono más grande
+              ),
+              label: const Text(
+                'Generar',
+                style: TextStyle(fontSize: 20), // Fuente más grande
+              ),
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Theme.of(context).colorScheme.secondary,
+                padding: const EdgeInsets.symmetric(vertical: 24, horizontal: 32),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(12),
+                ),
+              ),
+            ),
+
+            const SizedBox(height: 16),
+            Positioned(
+              top: 0,
+              right: 0,
+              child: IconButton(
+                icon: Icon(Icons.close, color: Theme.of(context).colorScheme.secondary, size: 28),
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
               ),
             ),
           ],
         ),
-      ],
+      ),
     );
   }
 }
